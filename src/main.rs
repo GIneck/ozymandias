2c895e0dbf67c13bcc80372c07434b2c2e43bbf6
GIneck <gjineck@protonmail.com>
2022-02-12 18:14:07 -0700

    Loops are working

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,25 @@ use std::io;
 use ozymandias::user_calls;
 
 fn main() {
-    let mut selection = String::new();
-        
+    let mut input = String::new();
+        loop {
     user_calls::begin_introductions();
 
     io::stdin()
-        .read_line(&mut selection)
+        .read_line(&mut input)
         .expect("Failed to read line");
     
-    let selection: u8 = selection.trim().parse().expect("Please type an Integer");
+    let selection: u8 = input.trim().parse().expect("Please type an Integer");
 
     match selection {
 
-//        0 => break,
+        0 => break,
         1 => user_calls::user_factorial(),
         2 => user_calls::user_fibonacchi(),
 
         _ => println!("Give me a proper selection please")
 
     }
-
+    input.drain(..);
+}
 }
--- src/user_calls.rs
+++ src/user_calls.rs
@@ -1,6 +1,5 @@
 use std::io;
 
-
 pub fn user_factorial(){
 
     println!();
